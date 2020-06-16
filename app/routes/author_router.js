2846d2eb37d55ac6177dda02f7ef020c06812b3e
Dimas Mokodompit <dimasdompit@gmail.com>
2020-06-16 16:58:36 +0700

    add authorization to author and genre router

--- app/routes/author_router.js
+++ app/routes/author_router.js
@@ -2,11 +2,16 @@ const express = require('express');
 const router = express.Router();
 const authorController = require('../controllers/controller_author');
 const upload = require('../helpers/upload');
+const {
+    is_Admin,
+    is_Staff,
+    is_User
+} = require('../middleware/auth_middleware');
 
-router.get('/', authorController.getAllAuthor);
-router.post('/', upload.none(), authorController.addAuthor);
-router.put('/:id', upload.none(), authorController.updateAuthor);
-router.delete('/:id', authorController.deleteAuthor);
-router.get('/search', authorController.searchAuthor);
+router.get('/', is_User, authorController.getAllAuthor);
+router.post('/', is_Staff, upload.none(), authorController.addAuthor);
+router.put('/:id', is_Staff, upload.none(), authorController.updateAuthor);
+router.delete('/:id', is_Admin, authorController.deleteAuthor);
+router.get('/search', is_User, authorController.searchAuthor);
 
 module.exports = router;
--- app/routes/genre_router.js
+++ app/routes/genre_router.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router();
 const genreController = require('../controllers/controller_genre');
 const upload = require('../helpers/upload');
+const {
+    is_Admin,
+    is_Staff,
+    is_User
+} = require('../middleware/auth_middleware');
 
-router.get('/', genreController.getAllGenre);
-router.post('/', upload.none(), genreController.addGenre);
-router.put('/:id', upload.none(), genreController.updateGenre);
-router.delete('/:id', genreController.deleteGenre);
+router.get('/', is_User, genreController.getAllGenre);
+router.post('/', is_Staff, upload.none(), genreController.addGenre);
+router.put('/:id', is_Staff, upload.none(), genreController.updateGenre);
+router.delete('/:id', is_Admin, genreController.deleteGenre);
 
-router.get('/search', genreController.searchGenre);
+router.get('/search', is_User, genreController.searchGenre);
 
 module.exports = router;
