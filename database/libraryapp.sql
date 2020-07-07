6658e2f3badf5190f9e0692071b652f484eb2151
Dimas Mokodompit <dimasdompit@gmail.com>
2020-07-07 17:02:10 +0700

    add new sql db

--- database/libraryapp.sql
+++ database/libraryapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 03 Jul 2020 pada 14.52
+-- Waktu pembuatan: 07 Jul 2020 pada 11.56
 -- Versi server: 10.4.13-MariaDB
 -- Versi PHP: 7.4.7
 
@@ -70,15 +70,16 @@ CREATE TABLE `book` (
 --
 
 INSERT INTO `book` (`id`, `title`, `description`, `image`, `genre_id`, `author_id`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'Harry Potter And The Deathly Hallows', 'Harry Potter, together with his teenage companions and the members of the Order of the Phoenix, enter the final struggle with Voldemort and his Death Eaters, without the guidance of Dumbledore. They seek to uncover and destroy Voldemort\'s horcruxes and the secrets of the Deathly Hallows.', 'deathly-hallows-1591969128434.jpg', 1, 1, 'Available', '2020-06-17 13:12:27', '2020-06-09 11:50:12'),
+(1, 'Harry Potter And The Deathly Hallows', 'Harry Potter, together with his teenage companions and the members of the Order of the Phoenix, enter the final struggle with Voldemort and his Death Eaters, without the guidance of Dumbledore. They seek to uncover and destroy Voldemort\'s horcruxes and the secrets of the Deathly Hallows.', 'deathly-hallows-1591969128434.jpg', 1, 1, 'Not Available', '2020-07-07 02:47:45', '2020-06-09 11:50:12'),
 (3, 'Murder on the Orient Express', 'A murder is discovered, and Poirot\'s trip home to London from the Middle East is interrupted to solve the murder. The US title of Murder in the Calais Coach was used to avoid confusion with the 1932 Graham Greene novel Stamboul Train, which had been published in the United States as Orient Express.', 'murder-on-the-orient-express-1592481208310.jpg', 4, 2, 'Available', '2020-07-02 04:59:39', '2020-06-09 18:11:35'),
 (6, 'Harry Potter and the Prisoner of Azkaban', 'A convicted murderer, Sirius Black, has broken out of Azkaban prison, and it seems he\'s after Harry. Now Hogwarts is being patrolled by the dementors, the Azkaban guards who are hunting Sirius. But Harry can\'t imagine that Sirius or, for that matter, the evil Lord Voldemort could be more frightening than the dem.', 'prisoner-of-azkaban-1591969244628.jpg', 1, 1, 'Not Available', '2020-06-12 13:40:44', '2020-06-10 10:31:27'),
 (7, 'The Murder of Roger Ackroyd', 'It is the third novel to feature Hercule Poirot as the lead detective. Poirot retires to a village near the home of a friend, Roger Ackroyd, to pursue a project to perfect vegetable marrows. Soon after, Ackroyd is murdered and Poirot must come out of retirement to solve the case.', 'murder-of-roger-ackroyd-1591969354426.jpg', 5, 2, 'Not Available', '2020-06-12 13:42:34', '2020-06-10 10:34:04'),
 (8, 'Captain Marvel', 'The first Captain Marvel printed by Marvel Comics was created by Stan Lee and Gene Colan. This character is an alien military officer, Captain Mar-Vell of the Kree Imperial Militia, who is sent to observe the planet Earth as it is developing technology to travel into space.', 'captain-marvel-1591969414599.jpg', 14, 5, 'Available', '2020-06-12 13:43:34', '2020-06-10 10:40:32'),
-(9, 'Iron Man', 'Iron Man is a fictional superhero appearing in American comic books published by Marvel Comics. ... Later, Stark develops his suit, adding weapons and other technological devices he designed through his company, Stark Industries. He uses the suit and successive versions to protect the world as Iron Man.', 'iron-man-1591969034879.jpg', 14, 5, 'Available', '2020-06-18 06:19:17', '2020-06-10 10:45:35'),
+(9, 'Iron Man', 'Iron Man is a fictional superhero appearing in American comic books published by Marvel Comics. ... Later, Stark develops his suit, adding weapons and other technological devices he designed through his company, Stark Industries. He uses the suit and successive versions to protect the world as Iron Man.', 'iron-man-1591969034879.jpg', 14, 5, 'Available', '2020-07-07 06:33:53', '2020-06-10 10:45:35'),
 (12, 'Mad, Bad & Dangerous to Know', 'Told in alternating narratives that bridge centuries, the latest novel from New York Times bestselling author Samira Ahmed traces the lives of two young women fighting to write their own stories and escape the pressure of familial burdens and cultural expectations in worlds too long defined by men.\n', 'mad-bad-dangerous-to-know-1591969493913.jpg', 9, 6, 'Not Available', '2020-07-01 20:52:34', '2020-06-11 07:22:09'),
-(14, 'The Hilarious World of Depression', 'For years John Moe, critically-acclaimed public radio personality and host of The Hilarious World of Depression podcast, struggled with depression; it plagued his family and claimed the life of his brother in 2007. As Moe came to terms with his own illness, he began to see similar patterns of behavior and coping mechanisms surfacing in conversations with others, including high-profile comedians who\'d struggled with the disease. Moe saw that there was tremendous comfort and community in open dialogue about these shared experiences and that humor had a unique power. Thus was born the podcast The Hilarious World of Depression.', 'the-hillarious-world-of-depression-1591969546325.jpg', 25, 8, 'Available', '2020-07-01 20:55:31', '2020-06-11 08:35:38'),
-(15, 'Officer Clemmons', 'Details the incredible life story of François Clemmons, beginning with his early years in Alabama and Ohio, marked by family trauma and loss, through his studies as a music major at Oberlin College, where Clemmons began to investigate and embrace his homosexuality, to a chance encounter with Fred Rogers which changed the whole course of both men’s lives, leading to a deep, spiritual friendship and mentorship spanning nearly forty years.', 'officer-clemmons-1591969587084.jpg', 25, 9, 'Not Available', '2020-07-02 03:36:43', '2020-06-11 08:42:23');
+(14, 'The Hilarious World of Depression', 'For years John Moe, critically-acclaimed public radio personality and host of The Hilarious World of Depression podcast, struggled with depression; it plagued his family and claimed the life of his brother in 2007. As Moe came to terms with his own illness, he began to see similar patterns of behavior and coping mechanisms surfacing in conversations with others, including high-profile comedians who\'d struggled with the disease. Moe saw that there was tremendous comfort and community in open dialogue about these shared experiences and that humor had a unique power. Thus was born the podcast The Hilarious World of Depression.', 'the-hillarious-world-of-depression-1591969546325.jpg', 25, 8, 'Not Available', '2020-07-07 02:54:41', '2020-06-11 08:35:38'),
+(15, 'Officer Clemmons', 'Details the incredible life story of François Clemmons, beginning with his early years in Alabama and Ohio, marked by family trauma and loss, through his studies as a music major at Oberlin College, where Clemmons began to investigate and embrace his homosexuality, to a chance encounter with Fred Rogers which changed the whole course of both men’s lives, leading to a deep, spiritual friendship and mentorship spanning nearly forty years.', 'officer-clemmons-1591969587084.jpg', 25, 9, 'Not Available', '2020-07-02 03:36:43', '2020-06-11 08:42:23'),
+(34, 'Lembur lagi ngerjain tugas', 'eijgijieogwhohngowrngowrbgowjrbgoubgrouwgouwrg gkwlr glwrg lwrg vbeubvobwrobubvowueevewvvwe', 'reactjs-img-1593848454494.png', 11, 8, 'Available', '2020-07-07 06:20:25', '2020-07-04 07:14:46');
 
 -- --------------------------------------------------------
 
@@ -164,7 +165,8 @@ INSERT INTO `users` (`id`, `username`, `password`, `roles_id`, `created_at`, `up
 (36, 'akuadalahuser2', '$2b$10$guOCJ0hWCHNJ9srm9.ZuIeBaLn.MqOwjHePIPJW61jO8kkbqcrAiS', 2, '2020-07-02 03:32:02', '2020-07-02 03:32:02'),
 (37, 'semuakarenadia', '$2b$10$T85u4Wte5DttE3WdSByciOgRaRUIhadYdfm0gzF.hp.JiCkJ0blDi', 2, '2020-07-02 18:44:31', '2020-07-02 18:44:31'),
 (38, 'semuakarenakamu', '$2b$10$NLGI6vZsybJIjzXNNiz1x.eNZ49F2Sh.RwY/SDkDwnduO9tFpJQxy', 2, '2020-07-02 18:45:02', '2020-07-02 18:45:02'),
-(39, 'akuadalahkamu', '$2b$10$2vwSUByTUQrxVEKV7DBLPOs12WcQakaTwZfslhd5kubDq5SN9kmnm', 2, '2020-07-02 18:45:52', '2020-07-02 18:45:52');
+(39, 'akuadalahkamu', '$2b$10$2vwSUByTUQrxVEKV7DBLPOs12WcQakaTwZfslhd5kubDq5SN9kmnm', 2, '2020-07-02 18:45:52', '2020-07-02 18:45:52'),
+(40, 'user1', '$2b$10$ZDqUnF2yB9j4P9hkHcARI.ZYAwaM3HYSDGqCqxPuIJZTF.pty2qw6', 2, '2020-07-04 12:46:04', '2020-07-04 12:46:04');
 
 --
 -- Indexes for dumped tables
@@ -217,7 +219,7 @@ ALTER TABLE `author`
 -- AUTO_INCREMENT untuk tabel `book`
 --
 ALTER TABLE `book`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT untuk tabel `genre`
@@ -235,7 +237,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
